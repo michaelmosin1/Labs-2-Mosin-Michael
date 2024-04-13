972f3aa0a02c5ddbec8793360dc346b13313afb2
Michael <michaelmosin777@gmail.com>
2024-04-13 12:14:57 +0200

    fixed some bugs with [] operator

--- 5lab/main.cpp
+++ 5lab/main.cpp
@@ -2,14 +2,54 @@
 
 
 int main(){
-    mm::Matrix<int> mat1(3, 3);
-    mm::Matrix<int> mat2(3, 3);
-    std::cin >> mat1 >> mat2;
-    std::cout << std::endl;
-    std::cout << mat1 << std::endl;
-    std::cout << mat2 << std::endl;
-    mat1+=mat2;
-    std::cout << mat1 << std::endl;
-    std::cout << mat2 << std::endl;
+    size_t n = 0;
+    size_t m = 0;
+    double x = 0;
+    short choose = 0;
+    std::cout << "Please, input size of matrix(rows, then columns): " << std::endl;
+    std::cin >> n >> m;
+    mm::Matrix<int> mat1(n, m);
+    mm::Matrix<int> mat2(n, m);
+    std::cout << "Please, input elements of your matrix: " << std::endl;
+    std::cin >> mat1;
+    return 0;
+    std::cout << "Please, choose your operation:\n1)+\t3)++\t5)A*B\t7)det(A)\n2)-\t4)--\t6)A*x" << std::endl;
+    std::cin >> choose;
+    switch (choose) {
+    case 1:
+        std::cout << "Please, input second matrix with same sizes as the first one" << std::endl;
+        std::cin >> mat2;
+        std::cout << mat1 + mat2 << std::endl;
+        break;
+    case 2:
+        std::cout << "Please, input second matrix with same sizes as the first one" << std::endl;
+        std::cin >> mat2;
+        std::cout << mat1 - mat2 << std::endl;
+        break;
+    case 3:
+        mat1++;
+        std::cout << mat1 << std::endl;
+        break;
+    case 4:
+        mat1--;
+        std::cout << mat1 << std::endl;
+        break;
+    case 5:
+        std::cout << "Please, input second matrix with same sizes as the first one" << std::endl;
+        std::cin >> mat2;
+        std::cout << mat1 - mat2 << std::endl;
+        break;
+    case 6:
+        std::cout << "Please, input x-multiplier" << std::endl;
+        std::cin >> x;
+        std::cout << mat1 * x << std::endl;
+        break;
+    case 7:
+        std::cout << mat1.det() << std::endl;
+        break;
+    default:
+        std::cerr << "Error! Wrong parameter of user`s choice" << std::endl;
+        break;
+    }
     return 0;
 }
--- 5lab/matrix.hpp
+++ 5lab/matrix.hpp
@@ -1,172 +1,207 @@
 #pragma once
 #include <iostream>
 
+namespace mm {
+template <typename T>
+class Matrix {
+private:
+    size_t size_m;
+    size_t size_n;
+    T** massive;
 
-namespace mm{
-    template <typename T>
-    class Matrix{
-        private:
-            size_t size_m;
-            size_t size_n;
-            T** massive;
-        public:
-            Matrix(){
-                this->size_m = 0;
-                this->size_n = 0;
-                this->massive = nullptr;
-            };
-            Matrix(size_t m, size_t n){
-                this->size_m = m;
-                this->size_n = n;
-                this->massive = new T*[size_m];
-                for (size_t i = 0; i < size_m; i++){
-                    this->massive[i] = new T[size_n];
-                }
-            };
-            Matrix(const Matrix& other){
-                this->size_m = other.size_m;
-                this->size_n = other.size_n;
-                this->massive = new T*[size_m];
-                for (size_t i = 0; i < size_m; i++){
-                    this->massive[i] = new T[size_n];
-                }
-                for (size_t i = 0; i < size_m; i++){
-                    for (size_t j = 0; j < size_n; j++){
-                        this->massive[i][j] = other.massive[i][j];
-                    }
-                }
-            };
-            ~Matrix(){
-                for (size_t i = 0; i < size_m; i++){
-                    delete[] this->massive[i];
-                }
-                delete[] this->massive;
-            };
-            Matrix& operator=(const Matrix& other){
-                this->size_m = other.size_m;
-                this->size_n = other.size_n;
-                this->massive = new T*[size_m];
-                for (size_t i = 0; i < size_m; i++){
-                    this->massive[i] = new T[size_n];
-                }
-                for (size_t i = 0; i < size_m; i++){
-                    for (size_t j = 0; j < size_n; j++){
-                        this->massive[i][j] = other.massive[i][j];
-                    }
-                }
-                return *this;
-            };
-            Matrix operator+(const Matrix& other){
-                Matrix result = *this;
-                result+=other;
-                return result;
-            };
-            Matrix& operator+=(const Matrix& other){
-                if ((this->size_m == other.size_m) && (this->size_m == other.size_n)){
-                    for (size_t i = 0; i < this->size_m; i++){
-                        for (size_t j = 0; j < this->size_n; j++){
-                            this->massive[i][j] += other.massive[i][j];
-                        }
-                    }
-                }
-                return *this;
-            };
-            Matrix operator-(const Matrix& other){
-                Matrix result = *this;
-                result-=other;
-                return result;
-            };
-            Matrix& operator-=(const Matrix& other){
-                if ((this->size_m == other.size_m) && (this->size_m == other.size_n)){
-                    for (size_t i = 0; i < size_m; i++){
-                        for (size_t j = 0; j < size_n; j++){
-                            this->massive[i][j] -= other.massive[i][j];
-                        }
-                    }
-                }
-                return *this;
-            };
-            Matrix operator*(const Matrix& other){
-                Matrix result = *this;
-                result*=other;
-                return result;
-            };
-            Matrix& operator*=(const Matrix& other){
-                if (this->size_n != other.size_m){
-                    std::cout << "Error! Can only multiply matrixes when number of first`s strings is equal to number of other`c columns" << std::endl;
-                    return *this;
-                }
-                Matrix result(this->size_m, other.size_n);
-                for (size_t i = 0; i < this->size_m; i++){
-                    for (size_t j = 0; j < other.size_n; j++){
-                        result.massive[i][j] = 0;
-                    }
-                }
-                for (size_t i = 0; i < this->size_m; i++){
-                    for (size_t j = 0; j < other.size_n; j++){
-                        for (size_t k = 0; k < this->size_n; k++){
-                            result.massive[i][j] += (this->massive[i][k]*other.massive[k][j]);
-                        }
-                    }
-                }
-                *this = result;
-                return *this;
-            };
-            Matrix operator*(const T& number){
-                Matrix result = *this;
-                result*=number;
-                return result;
-            };
-            Matrix& operator*=(const T& number){
-                for (size_t i = 0; i < size_m; i++){
-                    for (size_t j = 0; j < size_n; j++){
-                        this->massive[i][j] *= number;
-                    }
-                }
-                return *this;
-            };
-            Matrix& operator++(int){
-                for (size_t i = 0; i < size_m; i++){
-                    for (size_t j = 0; j < size_n; j++){
-                        this->massive[i][j] += 1;
-                    }
-                }
-                return *this;
-            };
-            Matrix& operator--(int){
-                for (size_t i = 0; i < size_m; i++){
-                    for (size_t j = 0; j < size_n; j++){
-                        this->massive[i][j] -= 1;
-                    }
-                }
-                return *this;
-            };
-            T operator[](size_t index) const {
-                return this->massive[index];
-            };
-            T& operator[](size_t index){
-                return this->massive[index];
-            };
-            double det(const Matrix& A){
-                return 0;
-            };
-
-            friend std::ostream& operator<<(std::ostream& out, const Matrix& A){
-                for (size_t i = 0; i < A.size_m; i++){
-                    for (size_t j = 0; j < A.size_n; j++){
-                        out << A.massive[i][j] << ' ';
-                    }
-                    out << std::endl;
-                }
-                return out;
-            };
-            friend std::istream& operator>>(std::istream& in, Matrix& A){
-                for (size_t i = 0; i < A.size_m; i++){
-                    for (size_t j = 0; j < A.size_n; j++){
-                        in >> A.massive[i][j];
-                    }
-                }
-                return in;
-            };
+public:
+    Matrix()
+    {
+        this->size_m = 0;
+        this->size_n = 0;
+        this->massive = nullptr;
     };
+    Matrix(size_t m, size_t n)
+    {
+        this->size_m = m;
+        this->size_n = n;
+        this->massive = new T*[size_m];
+        for (size_t i = 0; i < size_m; i++) {
+            this->massive[i] = new T[size_n];
+        }
+    };
+    Matrix(const Matrix& other)
+    {
+        this->size_m = other.size_m;
+        this->size_n = other.size_n;
+        this->massive = new T*[size_m];
+        for (size_t i = 0; i < size_m; i++) {
+            this->massive[i] = new T[size_n];
+        }
+        for (size_t i = 0; i < size_m; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                this->massive[i][j] = other.massive[i][j];
+            }
+        }
+    };
+    ~Matrix()
+    {
+        for (size_t i = 0; i < size_m; i++) {
+            delete[] this->massive[i];
+        }
+        delete[] this->massive;
+    };
+    Matrix& operator=(const Matrix& other)
+    {
+        this->size_m = other.size_m;
+        this->size_n = other.size_n;
+        this->massive = new T*[size_m];
+        for (size_t i = 0; i < size_m; i++) {
+            this->massive[i] = new T[size_n];
+        }
+        for (size_t i = 0; i < size_m; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                this->massive[i][j] = other.massive[i][j];
+            }
+        }
+        return *this;
+    };
+    Matrix operator+(const Matrix& other)
+    {
+        Matrix result = *this;
+        result += other;
+        return result;
+    };
+    Matrix& operator+=(const Matrix& other)
+    {
+        if ((this->size_m == other.size_m) && (this->size_m == other.size_n)) {
+            for (size_t i = 0; i < this->size_m; i++) {
+                for (size_t j = 0; j < this->size_n; j++) {
+                    this->massive[i][j] += other.massive[i][j];
+                }
+            }
+        }
+        return *this;
+    };
+    Matrix operator-(const Matrix& other)
+    {
+        Matrix result = *this;
+        result -= other;
+        return result;
+    };
+    Matrix& operator-=(const Matrix& other)
+    {
+        if ((this->size_m == other.size_m) && (this->size_m == other.size_n)) {
+            for (size_t i = 0; i < size_m; i++) {
+                for (size_t j = 0; j < size_n; j++) {
+                    this->massive[i][j] -= other.massive[i][j];
+                }
+            }
+        }
+        return *this;
+    };
+    Matrix operator*(const Matrix& other)
+    {
+        Matrix result = *this;
+        result *= other;
+        return result;
+    };
+    Matrix& operator*=(const Matrix& other)
+    {
+        if (this->size_n != other.size_m) {
+            std::cout << "Error! Can only multiply matrixes when number of first`s strings is equal to number of other`c columns" << std::endl;
+            return *this;
+        }
+        Matrix result(this->size_m, other.size_n);
+        for (size_t i = 0; i < this->size_m; i++) {
+            for (size_t j = 0; j < other.size_n; j++) {
+                result.massive[i][j] = 0;
+            }
+        }
+        for (size_t i = 0; i < this->size_m; i++) {
+            for (size_t j = 0; j < other.size_n; j++) {
+                for (size_t k = 0; k < this->size_n; k++) {
+                    result.massive[i][j] += (this->massive[i][k] * other.massive[k][j]);
+                }
+            }
+        }
+        *this = result;
+        return *this;
+    };
+    Matrix operator*(const T& number)
+    {
+        Matrix result = *this;
+        result *= number;
+        return result;
+    };
+    Matrix& operator*=(const T& number)
+    {
+        for (size_t i = 0; i < size_m; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                this->massive[i][j] *= number;
+            }
+        }
+        return *this;
+    };
+    Matrix& operator++(int)
+    {
+        for (size_t i = 0; i < size_m; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                this->massive[i][j] += 1;
+            }
+        }
+        return *this;
+    };
+    Matrix& operator--(int)
+    {
+        for (size_t i = 0; i < size_m; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                this->massive[i][j] -= 1;
+            }
+        }
+        return *this;
+    };
+    T* operator[](size_t index) const
+    {
+        return this->massive[index];
+    };
+    T*& operator[](size_t index)
+    {
+        return this->massive[index];
+    };
+    T det()
+    {
+        if (size_m != size_n) {
+            throw std::logic_error("Only square matrixes have determinant!\n");
+        }
+        mm::Matrix copy = *this;
+        double determinant = 1.0;
+        for (size_t i = 0; i < size_n - 1; i++) {
+            for (size_t j = 0; j < size_n; j++) {
+                double coefficient = (copy[i + 1][0] * 1.0) / copy[i][0];
+                for (size_t k = 0; k < size_n; k++) {
+                    copy[i + 1][j] -= (copy[i][j] * coefficient);
+                }
+            }
+            determinant *= copy[i][i];
+        }
+        std::cout << copy << std::endl;
+        determinant *= copy[size_n - 1][size_n - 1];
+        return determinant;
+    };
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& A)
+    {
+        for (size_t i = 0; i < A.size_m; i++) {
+            for (size_t j = 0; j < A.size_n; j++) {
+                out << A.massive[i][j] << ' ';
+            }
+            out << std::endl;
+        }
+        return out;
+    };
+    friend std::istream& operator>>(std::istream& in, Matrix& A)
+    {
+        for (size_t i = 0; i < A.size_m; i++) {
+            for (size_t j = 0; j < A.size_n; j++) {
+                in >> A.massive[i][j];
+            }
+        }
+        return in;
+    };
+};
 }
